eae0fbc5b1a5328815bbc46d2a4e12fb63e439f1
md <75916680+mdelgado96@users.noreply.github.com>
2022-03-06 17:59:51 -0800

    index.js update

--- src/index.js
+++ src/index.js
@@ -10,13 +10,14 @@ import { BrowserRouter } from 'react-router-dom';
 library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faArrowLeft);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-export default App;
